19c22c54127090fd5d7f26035684c9194b7f0e30
Austin Rivas <austinrivas@gmail.com>
2020-06-18 00:39:05 +0700

    cargo fmt

--- template/rust-hasura-action/main/src/main.rs
+++ template/rust-hasura-action/main/src/main.rs
@@ -2,10 +2,8 @@ use actix_web::{App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(function::handler)
-    })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(function::handler))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }

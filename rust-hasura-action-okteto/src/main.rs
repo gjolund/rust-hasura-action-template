339fb6d13d336c85d082b3c451ac975862e14dd9
Austin Rivas <austinrivas@gmail.com>
2020-06-18 17:23:13 +0700

    added live reload to okteto image

---
+++ rust-hasura-action-okteto/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{App, HttpServer};
+use listenfd::ListenFd;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().service(function::handler));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:3000")?
+    };
+
+    server.run().await
+}

f732cdcd63ee295582df58a9d21b63b50a1ff9c8
Austin Rivas <austinrivas@gmail.com>
2020-06-18 14:58:15 +0700

    added test for handler

--- template/rust-hasura-action/function/src/lib.rs
+++ template/rust-hasura-action/function/src/lib.rs
@@ -2,13 +2,30 @@ use actix_web::{get, HttpResponse, Responder};
 
 #[get("/")]
 pub async fn handler() -> impl Responder {
-    HttpResponse::Ok().body("Hello Actix!")
+    HttpResponse::Ok().body("Hello Actix World!")
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::*;
+    use actix_web::{http::StatusCode, test, App};
+    use actix_http::Request;
+    use bytes::Bytes;
+
+    fn create_req() -> Request {
+      test::TestRequest::with_uri("/").to_request()
+    }
+
+    #[actix_rt::test]
+    async fn test_handler() {
+        let mut app = test::init_service(App::new().service(handler)).await;
+
+        let req = create_req();
+        let resp = test::call_service(&mut app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+
+        let req = create_req();
+        let result = test::read_response(&mut app, req).await;
+        assert_eq!(result, Bytes::from_static(b"Hello Actix World!"));
     }
 }
